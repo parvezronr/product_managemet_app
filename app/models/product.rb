ab8135a946d7f062e1fd8752b83392984efd4b78
parvezronr <syedparvezronr@gmail.com>
2023-12-02 17:54:30 +0530

    Added controller logig and modified routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+	after_update :send_status_change_notification, if: :saved_change_to_active?
+
+  private
+
+  def send_status_change_notification
+  	byebug
+    users_to_notify = User.where(admin: false)
+    users_to_notify.each do |user|
+    	byebug
+      ProductNotifierMailer.status_change_notification(user, self, active).deliver_now
+    end
+  end
 end
